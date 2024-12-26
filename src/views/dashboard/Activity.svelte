e5b1b07a3a4a63e3c509242ede09206b2c7b73fa
Thomas Melak <melakthomas@gmail.com>
2024-12-26 06:15:34 +0300

    Mobile Version for activity done

--- src/views/dashboard/Activity.svelte
+++ src/views/dashboard/Activity.svelte
@@ -1,23 +1,29 @@
 <script lang="ts">
-    import CardTable, { type TableColumn } from "components/Cards/CardTable.svelte";
+    import CardTable, {type TableColumn} from "components/Cards/CardTable.svelte";
     import TableDropdown from "components/Dropdowns/TableDropdown.svelte";
-    import { fly } from "svelte/transition";
-    import { staggerdTransition } from "utils/animationTools";
+    import {fly} from "svelte/transition";
+    import {staggerdTransition} from "utils/animationTools";
+    import {MediaQuery} from "svelte/reactivity";
+    import {currencyFormat} from "utils/formatTools";
+
+
+    let mobile = new MediaQuery("max-width: 1024px");
+
     const columns: TableColumn<Record<string, any>>[] = [
-    
-    { key: "Date", header: "Date", sortable: true },
-    { key: "Transaction", header: "Description", sortable: true },
-    { key: "name", header: "Security | Ticker", sortable: true },
-    { key: "Quantity", header: "Quantity (Shares)", sortable: true },
-    { key: "Price", header: "Price (Etb)", sortable: true },
-    { key: "netAmount", header: "Net Amount (Etb)", sortable: true },
-    {
-      key: "id",
-      header: "",
-      type: "action",
-        action: (a) => console.table(a),
-    },
-  ];
+
+        {key: "Date", header: "Date", sortable: true},
+        {key: "Transaction", header: "Description", sortable: true},
+        {key: "name", header: "Security | Ticker", sortable: true},
+        {key: "Quantity", header: "Quantity (Shares)", sortable: true},
+        {key: "price", header: "Price (Etb)", sortable: true},
+        {key: "netAmount", header: "Net Amount (Etb)", sortable: true},
+        {
+            key: "id",
+            header: "",
+            type: "action",
+            action: (a) => console.table(a)
+        }
+    ];
 
     const data = [
         {
@@ -26,7 +32,7 @@
             "name": "Ethio Telecom | ETC",
             "Quantity": "3.00",
             "symbol": "ETC",
-            "Price": "400.00",
+            "price": "400.00",
             "netAmount": "-1,200.00"
         }, {
             "Date": "Dec 19-2024",
@@ -34,7 +40,7 @@
             "name": "Commercial Bank of Ethiopia | CBE",
             "Quantity": "4.00",
             "symbol": "CBE",
-            "Price": "890.00",
+            "price": "890.00",
             "netAmount": "3,560.00"
         }, {
             "Date": "Nov 22-2024",
@@ -42,15 +48,47 @@
             "name": "Ethiopian Insurance Corporation | EIC",
             "Quantity": "8.00",
             "symbol": "EIC",
-            "Price": "280.00",
+            "price": "280.00",
             "netAmount": "2,240.00"
         }
     ];
 </script>
-{#snippet actionSnippet(value:Record<string, any>)}
-  <TableDropdown value={value}/>
+{#snippet actionSnippet(value: Record<string, any>)}
+    <TableDropdown value={value}/>
 {/snippet}
 <div class="w-full z-3" in:fly|global={staggerdTransition(1)}>
-    <CardTable {columns} {data} title={'Activity'} {actionSnippet}/>
+
+
+    {#if mobile.current}
+        <h3 class="font-semibold text-lg capitalize mb-2">
+            Activity
+            {#if data.length > 0}
+                ({data.length})
+            {/if}
+        </h3>
+
+        <div class="flex flex-col glass-effect">
+            {#each data as security}
+                <div class="flex w-full justify-between p-4">
+                    <div class="flex gap-2">
+                        <div class="flex flex-col">
+                            <span>{security.Date}</span>
+                            <span>{security.name}</span>
+                            <span>{security.Transaction}</span>
+
+                        </div>
+
+                    </div>
+                    {@render actionSnippet(security)}
+                </div>
+
+
+            {/each}
+        </div>
+
+
+    {:else}
+        <CardTable {columns} {data} title={'Activity'} {actionSnippet}/>
+    {/if}
 </div>
 
