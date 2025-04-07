b877ab608edc11b8bc9fb9535e8a503f20d3c589
Thomas Melak <me@thomasmelak.io>
2025-04-08 00:57:03 +0300

    Improve search behaviour.

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,91 +1,98 @@
-<div class="md:justify-self-end w-full relative">
-  <BaseInput
-    inputClass="py-4"
-    placeholder="Search stock"
-    prependIcon="search"
-    oninput={handleSearch}
-    onfocusout={() => setTimeout(reset, 500)}
-    bind:value={searchTerm}
-  ></BaseInput>
-
-  {#if searching || securities.length > 0}
-    <div class="card w-full absolute text-left rounded-t-none">
-      {#if searching}
-        <div class="p-2">
-          <Loading />
-        </div>
-      {:else if securities.length > 0}
-        <div class="flex flex-col">
-          {#each securities as security}
-            <button
-              onclick={() => openTearsheet(security)}
-              class="w-full py-2 px-4 hover:bg-accent/30 dark:hover:bg-accent-dark/30 border-neutral-500 border-b border-opacity-50"
-            >
-              <span class="flex justify-between sm:flex-col text-left">
-                <strong>{security.symbol}</strong>
-                <span>{security.name}</span>
-              </span>
-            </button>
-          {/each}
-        </div>
-      {/if}
-    </div>
-  {/if}
-
-  <Tearsheet bind:this={tearsheetModal} />
-</div>
-
 <script lang="ts">
   import { findSecurities } from '@/api/api_security'
   import BaseInput from 'components/Inputs/BaseInput.svelte'
   import Tearsheet from 'components/Modals/Tearsheet.svelte'
   import type { ISecurity } from 'models/security'
-  import type { SvelteComponent } from 'svelte'
-  import Loading from './Loading.svelte'
+  import { type SvelteComponent } from 'svelte'
+  import { circInOut } from 'svelte/easing'
+  import { slide } from 'svelte/transition'
 
-  let tearsheetModal: SvelteComponent | undefined = $state();
+  let tearsheetModal: SvelteComponent | undefined = $state()
 
-  let searchTerm = $state('');
-  let securities: ISecurity[] = $state([]);
-  let timeout: NodeJS.Timeout;
-  let searching = $state(false);
+  let searchTerm = $state('')
+  let securities: ISecurity[] = $state([])
+  let timeout: NodeJS.Timeout
+  let searching = $state(false)
 
   const openTearsheet = (data: ISecurity) => {
-    tearsheetModal?.show(undefined, data.id);
-  };
+    tearsheetModal?.show(undefined, data.id)
+  }
 
   async function getProducts() {
     if (!searchTerm) {
-      reset();
-      return;
+      reset()
+      return
     }
 
-    const result = await findSecurities(searchTerm);
-    searching = false;
+    const result = await findSecurities(searchTerm)
+    searching = false
 
     if (result.error) {
-      handleError();
+      handleError()
     }
 
     if (result.data) {
-      securities = result.data.response;
+      securities = result.data.response
     }
   }
 
   function handleSearch() {
-    searching = true;
+    searching = true
     if (timeout) {
-      clearTimeout(timeout);
+      clearTimeout(timeout)
     }
-    timeout = setTimeout(getProducts, 500);
+    timeout = setTimeout(getProducts, 500)
   }
 
   function handleError() {
-    reset();
+    reset()
   }
 
   function reset() {
-    securities = [];
-    searching = false;
+    securities = []
+    searchTerm = ''
+    searching = false
   }
 </script>
+
+<div
+  class="md:justify-self-end w-full relative"
+  onfocusoutcapture={async (e) => {
+    if (!e.currentTarget.contains(e.relatedTarget as Node)) {
+      reset()
+    }
+  }}>
+  <BaseInput
+    inputClass="py-4"
+    placeholder="Search stock"
+    prependIcon="search"
+    oninput={handleSearch}
+    onfocusout={(e) => e.preventDefault}
+    bind:value={searchTerm}></BaseInput>
+
+  {#if !searching && securities.length > 0}
+    <div
+      class="card w-full absolute text-left rounded-t-none z-20 top-[90%]"
+      transition:slide={{
+        axis: 'y',
+        easing: circInOut,
+        duration: 200,
+      }}>
+      <div class="flex flex-col">
+        {#each securities as security}
+          <button
+            tabindex="0"
+            onclick={() => openTearsheet(security)}
+            class="w-full py-2 px-4 hover:bg-accent/30 dark:hover:bg-accent-dark/30 border-neutral-500 border-b border-opacity-50">
+            <span class="flex justify-between sm:flex-col text-left">
+              <strong>{security.symbol}</strong>
+              <span>{security.name}</span>
+            </span>
+          </button>
+        {/each}
+      </div>
+    </div>
+  {/if}
+
+  <Tearsheet bind:this={tearsheetModal} />
+</div>
